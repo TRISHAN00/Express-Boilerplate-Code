56017f8fc52798eca79344fb8d0d0af1b7b68212
TRISHAN00 <trishansaha43@gmail.com>
2022-10-19 17:48:11 +0600

    complete boilerplate of express project

--- app/app.js
+++ app/app.js
@@ -1,8 +1,14 @@
+require("dotenv").config("../.env");
 const express = require("express");
+const middleware = require("./middleware");
+const router = require("./routes");
+const { notFoundHandler, errorHandler } = require("./error");
+
 const app = express();
 
-app.get("/health", (_req, res) => {
-  res.status(200).json({ message: "success" });
-});
+app.use(middleware);
+app.use(router);
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 module.exports = app;
